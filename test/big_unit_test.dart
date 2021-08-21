aa1393515264a2488ac9ac20b792f96a54039c17
Murashu7 <murashu7@gmail.com>
2021-08-21 17:39:21 +0900

    API初期化コード修正

--- test/big_unit_test.dart
+++ test/big_unit_test.dart
@@ -6,49 +6,53 @@ import 'package:small_test_sample/item_checker.dart';
 import 'package:small_test_sample/user_checker.dart';
 
 void main() {
-  test("big_unit_test", () {
-    var api = Api(
+  Api? api;
+
+  setUp(() {
+    api = Api(
       ItemCheckerImpl(),
       UserCheckerImpl(
         AgeCheckerImpl(),
         AddressCheckerImpl(),
       ),
     );
+  });
 
+  test("big_unit_test", () {
     DB.stock["iPhoneSE"] = 3;
-    expect(api.check("iPhoneSE", 20, "tokyo"), "OK");
+    expect(api!.check("iPhoneSE", 20, "tokyo"), "OK");
 
     DB.stock.clear();
-    expect(api.check("iPhoneSE", 20, "tokyo"), "NG ITEM");
+    expect(api!.check("iPhoneSE", 20, "tokyo"), "NG ITEM");
 
     DB.stock["iPhone8"] = 3;
-    expect(api.check("iPhoneSE", 20, "tokyo"), "NG ITEM");
+    expect(api!.check("iPhoneSE", 20, "tokyo"), "NG ITEM");
 
     DB.stock["iPhoneSE"] = 3;
-    expect(api.check("iPhoneSE", 19, "tokyo"), "NG USER");
+    expect(api!.check("iPhoneSE", 19, "tokyo"), "NG USER");
 
     DB.stock["iPhoneSE"] = 3;
-    expect(api.check("iPhoneSE", 20, "osaka"), "NG USER");
+    expect(api!.check("iPhoneSE", 20, "osaka"), "NG USER");
 
     DB.stock.clear();
-    expect(api.check("iPhoneSE", 19, "tokyo"), "NG ITEM");
+    expect(api!.check("iPhoneSE", 19, "tokyo"), "NG ITEM");
 
     DB.stock["iPhone8"] = 3;
-    expect(api.check("iPhoneSE", 19, "tokyo"), "NG ITEM");
+    expect(api!.check("iPhoneSE", 19, "tokyo"), "NG ITEM");
 
     DB.stock.clear();
-    expect(api.check("iPhoneSE", 20, "osaka"), "NG ITEM");
+    expect(api!.check("iPhoneSE", 20, "osaka"), "NG ITEM");
 
     DB.stock["iPhone8"] = 3;
-    expect(api.check("iPhoneSE", 20, "osaka"), "NG ITEM");
+    expect(api!.check("iPhoneSE", 20, "osaka"), "NG ITEM");
 
     DB.stock["iPhoneSE"] = 3;
-    expect(api.check("iPhoneSE", 19, "osaka"), "NG USER");
+    expect(api!.check("iPhoneSE", 19, "osaka"), "NG USER");
 
     DB.stock.clear();
-    expect(api.check("iPhoneSE", 19, "osaka"), "NG ITEM");
+    expect(api!.check("iPhoneSE", 19, "osaka"), "NG ITEM");
 
     DB.stock["iPhone8"] = 3;
-    expect(api.check("iPhoneSE", 19, "osaka"), "NG ITEM");
+    expect(api!.check("iPhoneSE", 19, "osaka"), "NG ITEM");
   });
 }
