279b2e58245406a2c293005a06aeaf4d80bf99f2
Murashu7 <murashu7@gmail.com>
2021-08-22 17:13:23 +0900

    小さいユニットテスト実装

--- test/small_unit_test.dart
+++ test/small_unit_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:small_test_sample/address_checker.dart';
 import 'package:small_test_sample/age_checker.dart';
+import 'package:small_test_sample/api.dart';
 import 'package:small_test_sample/item_checker.dart';
+import 'package:small_test_sample/user_checker.dart';
 
 void main() {
   group("small_unit_test", () {
@@ -24,6 +26,64 @@ void main() {
     test("AddressCheckerTest", () {
       assert(AddressCheckerImpl().check("tokyo"));
       assert(!AddressCheckerImpl().check("osaka"));
+    });
+
+    test("UserCheckTest", () {
+      // Mockを用意する
+      var ageOk = () {
+        return AnonymousAgeChecker(
+          check: (int age) => true,
+        );
+      };
+      var ageNg = () {
+        return AnonymousAgeChecker(
+          check: (int age) => false,
+        );
+      };
+      var addressOk = () {
+        return AnonymousAddressChecker(
+          check: (String address) => true,
+        );
+      };
+      var addressNg = () {
+        return AnonymousAddressChecker(
+          check: (String address) => false,
+        );
+      };
+
+      assert(UserCheckerImpl(ageOk(), addressOk()).check(20, "tokyo"));
+      assert(!UserCheckerImpl(ageNg(), addressOk()).check(19, "tokyo"));
+      assert(!UserCheckerImpl(ageOk(), addressNg()).check(20, "osaka"));
+      assert(!UserCheckerImpl(ageNg(), addressNg()).check(19, "osaka"));
+    });
+
+    test("ApiTest", () {
+      // Mockを用意する
+      var itemOk = () {
+        return AnonymousItemChecker(
+          check: (String item) => true,
+        );
+      };
+      var itemNg = () {
+        return AnonymousItemChecker(
+          check: (String item) => false,
+        );
+      };
+      var userOk = () {
+        return AnonymousUserChecker(
+          check: (int age, String address) => true,
+        );
+      };
+      var userNg = () {
+        return AnonymousUserChecker(
+          check: (int age, String address) => false,
+        );
+      };
+
+      expect(Api(itemOk(), userOk()).check("iPhoneSE", 20, "tokyo"), "OK");
+      expect(Api(itemNg(), userOk()).check("iPhoneSE", 20, "tokyo"), "NG ITEM");
+      expect(Api(itemOk(), userNg()).check("iPhoneSE", 20, "tokyo"), "NG USER");
+      expect(Api(itemNg(), userNg()).check("iPhoneSE", 20, "tokyo"), "NG ITEM");
     });
   });
 }
